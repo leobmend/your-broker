8ac2f09fba2d4006078b8bbacfbd080a9faff021
leobmend <leo.bmendonca@gmail.com>
2022-07-15 16:07:18 -0300

    feat: add database configuration and model instance

---
+++ src/database/config/database.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { Dialect, Options } from 'sequelize';
+
+const config: Options = {
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  dialect: process.env.DATABASE_DIALECT as Dialect,
+};
+
+export = config;
