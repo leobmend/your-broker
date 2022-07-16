5cfa4d5181e41aa34ed3c950cc7c0d0b675b941d
leobmend <leo.bmendonca@gmail.com>
2022-07-16 19:12:36 +0000

    feat: add ativos controller

---
+++ src/controllers/ativos.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import ativosService from '../services/ativos.service';
+
+const getAtivos = async (_req: Request, res: Response): Promise<Response> => {
+  const ativos = await ativosService.getAll();
+  return res.status(StatusCodes.OK).json(ativos);
+};
+
+const ativosController = {
+  getAtivos,
+};
+
+export default ativosController;
