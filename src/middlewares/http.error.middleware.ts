4181a50e897cd3b282f2d6cbc998cb6a6231bb23
leobmend <leo.bmendonca@gmail.com>
2022-07-16 19:40:52 +0000

    fix: add next parameter to http error middleware definition

--- src/middlewares/http.error.middleware.ts
+++ src/middlewares/http.error.middleware.ts
@@ -1,8 +1,8 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import HttpException from '../utils/HttpError';
 
-const httpErrorMiddleware = (err: Error, req: Request, res: Response) => {
+const httpErrorMiddleware = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
   const { status, message } = err as HttpException;
   res.status(status || StatusCodes.INTERNAL_SERVER_ERROR).json({ message });
 };
