bd0d1ca59ba772fc70f52092913a281f2730a47c
leobmend <leo.bmendonca@gmail.com>
2022-07-16 21:04:37 +0000

    feat: add "clientes" controller

---
+++ src/controllers/clientes.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import clientesService from '../services/clientes.service';
+
+const getClientes = async (_req: Request, res: Response): Promise<Response> => {
+  const clientes = await clientesService.getAll();
+  return res.status(StatusCodes.OK).json(clientes);
+};
+
+const clientesController = {
+  getClientes,
+};
+
+export default clientesController;
