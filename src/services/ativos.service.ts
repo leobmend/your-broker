15a005ac09f6f00839eb938971827b69c6c3031e
leobmend <leo.bmendonca@gmail.com>
2022-07-16 19:10:29 +0000

    feat: add ativos service

---
+++ src/services/ativos.service.ts
@@ -0,0 +1,29 @@
+import { StatusCodes } from 'http-status-codes';
+import { IAtivo } from '../database/interfaces/ativos.interface';
+import Ativo from '../database/models/ativos.model';
+import HttpError from '../utils/HttpError';
+
+const getAll = async (): Promise<IAtivo[]> => {
+  const ativos = await Ativo.findAll();
+  return ativos;
+};
+
+const getByCodB3 = async (codAtivoB3: string): Promise<IAtivo | null> => {
+  const ativo = await Ativo.findOne({ where: { codAtivoB3 } });
+  return ativo;
+};
+
+const create = async (ativo: Omit<IAtivo, 'codAtivo'>): Promise<IAtivo> => {
+  const ativoByCodB3 = await getByCodB3(ativo.codAtivoB3);
+  if (ativoByCodB3) throw new HttpError(StatusCodes.CONFLICT, 'Já existe ativo com este código B3');
+
+  const newAtivo = await Ativo.create(ativo);
+  return newAtivo;
+};
+
+const ativosService = {
+  getAll,
+  create,
+};
+
+export default ativosService;
