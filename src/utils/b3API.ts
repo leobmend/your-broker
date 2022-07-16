6f963ef8daeb5ec7db7ed552393c61a4b8fe96d3
leobmend <leo.bmendonca@gmail.com>
2022-07-16 15:16:39 +0000

    refactor: change getAcoes method name for getAtivos on b3API util

--- src/utils/b3API.ts
+++ src/utils/b3API.ts
@@ -5,7 +5,7 @@ const b3API: AxiosInstance = axios.create({
   baseURL: 'https://api-cotacao-b3.labdo.it/api',
 });
 
-export const getAcoes = async (): Promise<IGetAcoesReturn[] | void> => (
+export const getAtivos = async (): Promise<IGetAcoesReturn[] | void> => (
   b3API.get('/carteira')
     .then((response) => (
       response.data.map(
