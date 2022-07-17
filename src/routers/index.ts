74c95ecd01fc3c8da097b360036c9c8bfa921280
leobmend <leo.bmendonca@gmail.com>
2022-07-17 12:15:50 +0000

    feat: add "operacoes" router and apply it

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import ativosRouter from './ativos.router';
 import clientesRouter from './clientes.router';
 import investimentosRouter from './investimentos.router';
+import operacoesRouter from './operacoes.router';
 import transacoesRouter from './transacoes.router';
 
 const router = express.Router();
@@ -11,5 +12,6 @@ router.use('/clientes', clientesRouter);
 router.use('/ativos', ativosRouter);
 router.use('/investimentos', investimentosRouter);
 router.use('/transacoes', transacoesRouter);
+router.use('/operacoes', operacoesRouter);
 
 export default router;
