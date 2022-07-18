438f6fdf512a83c7512da21070bdc1881356ba40
leobmend <leo.bmendonca@gmail.com>
2022-07-18 02:02:25 +0000

    feat: apply authentication middleware to routes that need it

--- src/routers/operacoes.router.ts
+++ src/routers/operacoes.router.ts
@@ -1,20 +1,25 @@
 import { Router } from 'express';
+
 import operacoesController from '../controllers/operacoes.controller';
+import authenticationMiddleware from '../middlewares/authentication.middleware';
 
 const operacoesRouter = Router();
 
 operacoesRouter.get(
   '/:codCliente',
+  authenticationMiddleware,
   operacoesController.getByCliente,
 );
 
 operacoesRouter.post(
   '/comprar',
+  authenticationMiddleware,
   operacoesController.comprar,
 );
 
 operacoesRouter.post(
   '/vender',
+  authenticationMiddleware,
   operacoesController.vender,
 );
 
--- src/routers/transacoes.router.ts
+++ src/routers/transacoes.router.ts
@@ -1,20 +1,25 @@
 import { Router } from 'express';
+
 import transacoesController from '../controllers/transacoes.controller';
+import authenticationMiddleware from '../middlewares/authentication.middleware';
 
 const transacoesRouter = Router();
 
 transacoesRouter.get(
   '/:codCliente',
+  authenticationMiddleware,
   transacoesController.getByCliente,
 );
 
 transacoesRouter.post(
   '/deposito',
+  authenticationMiddleware,
   transacoesController.deposito,
 );
 
 transacoesRouter.post(
   '/saque',
+  authenticationMiddleware,
   transacoesController.saque,
 );
 
