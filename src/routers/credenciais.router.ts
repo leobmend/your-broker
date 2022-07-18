2b8b6034a96627a46203154e29a6f9900ad96eda
leobmend <leo.bmendonca@gmail.com>
2022-07-18 14:07:23 +0000

    feat: add "credenciais" validations middlewares and apply them to proper routes

--- src/routers/credenciais.router.ts
+++ src/routers/credenciais.router.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express';
+
 import credenciaisController from '../controllers/credenciais.controller';
+import {
+  loginValidationMiddleware, createAccountValidationMiddleware,
+} from '../middlewares/credenciais.validation.middleware';
 
 const credenciaisRouter = Router();
 
 credenciaisRouter.post(
   '/cadastrar',
+  createAccountValidationMiddleware,
   credenciaisController.createAccount,
 );
 
 credenciaisRouter.post(
   '/entrar',
+  loginValidationMiddleware,
   credenciaisController.login,
 );
 
