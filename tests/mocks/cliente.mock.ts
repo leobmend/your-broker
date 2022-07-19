c25f0e406a1d683565058b17cef0b11e4dafe79e
leobmend <leo.bmendonca@gmail.com>
2022-07-19 02:40:57 +0000

    feat: add "cliente" tests mocks

---
+++ tests/mocks/cliente.mock.ts
@@ -0,0 +1,20 @@
+export const clienteGetMock = {
+  codCliente: 1,
+  nome: 'John Doe',
+  email: 'test@test.com',
+  saldo: 10.5,
+};
+
+export const clientePostMock = {
+  nome: 'John Doe',
+  email: 'test@test.com',
+  senha: 'test-password',
+};
+
+export const clienteFullMock = {
+  codCliente: 1,
+  nome: 'John DOe',
+  email: 'test@test.com',
+  saldo: 0,
+  senha: 'test-password',
+};
