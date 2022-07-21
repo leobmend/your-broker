389484291a6ed6258ad5eda3f3a5bb37f3cae636
leobmend <leo.bmendonca@gmail.com>
2022-07-21 23:04:53 +0000

    refactor: add relationships to "movimentos" model and "tipo" columns to "operacoes" and "transacoes" models

--- src/database/models/movimentos.model.ts
+++ src/database/models/movimentos.model.ts
@@ -1,8 +1,5 @@
 import { Model, STRING } from 'sequelize';
 import db from '.';
-import Operacao from './operacoes.model';
-
-import Transacao from './transacoes.model';
 
 class Movimento extends Model {
   codAtivo!: string;
@@ -23,37 +20,5 @@ Movimento.init({
   modelName: 'movimentos',
   timestamps: false,
 });
-
-Transacao.belongsTo(
-  Movimento,
-  {
-    as: 'movimentoTransacao',
-    foreignKey: 'tipo',
-  },
-);
-
-Movimento.hasMany(
-  Transacao,
-  {
-    as: 'transacoes',
-    foreignKey: 'tipo',
-  },
-);
-
-Operacao.belongsTo(
-  Movimento,
-  {
-    as: 'movimentoOperacao',
-    foreignKey: 'tipo',
-  },
-);
-
-Movimento.hasMany(
-  Operacao,
-  {
-    as: 'operacoes',
-    foreignKey: 'tipo',
-  },
-);
 
 export default Movimento;
