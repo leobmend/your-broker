0b59cb474b3986ecda303888b06d8b248e749ded
leobmend <leo.bmendonca@gmail.com>
2022-07-22 15:01:53 +0000

    refactor: remove swagger docs specifications from routers files and write a single swagger.json file, containing all the docs specifications

--- src/routers/credenciais.router.ts
+++ src/routers/credenciais.router.ts
@@ -8,96 +8,11 @@ import {
 
 const credenciaisRouter = Router();
 
-/**
- * @swagger
- *  components:
- *    schemas:
- *      Novo Cliente:
- *        type: object
- *        required:
- *          - nome
- *          - email
- *          - senha
- *        properties:
- *          nome:
- *            type: string
- *          email:
- *            type: string
- *          senha:
- *            type: string
- *        example:
- *          nome: John Doe
- *          email: john@test.com
- *          senha: "123123"
- *
- *      Login:
- *        type: object
- *        required:
- *          - email
- *          - senha
- *        properties:
- *          email:
- *            type: string
- *          senha:
- *            type: string
- *        example:
- *          email: john@test.com
- *          senha: "123123"
- *
- *      Token:
- *        type: string
- *        example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9[...]
-*/
-
-/**
- * @swagger
- *  /credenciais/cadastrar:
- *    post:
- *      tags: [Credenciais]
- *      description: Cadastra o cliente
- *      requestBody:
- *        required: true
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              $ref: '#/components/schemas/Novo Cliente'
- *      responses:
- *        201:
- *          content:
- *            application/json:
- *              schema:
- *                type: object
- *                $ref: '#/components/schemas/Token'
-*/
-
 credenciaisRouter.post(
   '/cadastrar',
   createAccountValidationMiddleware,
   credenciaisController.createAccount,
 );
-
-/**
- * @swagger
- *  /credenciais/entrar:
- *    post:
- *      tags: [Credenciais]
- *      description: Loga o cliente
- *      requestBody:
- *        required: true
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              $ref: '#/components/schemas/Login'
- *      responses:
- *        201:
- *          content:
- *            application/json:
- *              schema:
- *                type: object
- *                $ref: '#/components/schemas/Token'
-*/
 
 credenciaisRouter.post(
   '/entrar',
